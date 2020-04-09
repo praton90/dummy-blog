51230a2c40ecda94a5ef232253c98700f3236cdb
praton90 <pablo.raton@goeuro.com>
2020-04-09 15:29:42 +0200

    Updates

---
+++ precache-manifest.894c1e79c9a7984d983d87e6fea789ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40c73e90eacd3208a8228e70fcbad11",
+    "url": "/dummy-blog/index.html"
+  },
+  {
+    "revision": "d080acf2e939648bbac1",
+    "url": "/dummy-blog/static/css/main.5006220b.chunk.css"
+  },
+  {
+    "revision": "2dcfc8a585ca5bbff8f2",
+    "url": "/dummy-blog/static/js/2.6a9f51db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dummy-blog/static/js/2.6a9f51db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d080acf2e939648bbac1",
+    "url": "/dummy-blog/static/js/main.39caa444.chunk.js"
+  },
+  {
+    "revision": "7f3857f3e6fa0519479b",
+    "url": "/dummy-blog/static/js/runtime-main.adcaec3a.js"
+  }
+]);
